c0877d8db67721c207000892e18486d8a74acfe5
tawfikm <tawfikmourchid@gmail.com>
2025-10-17 01:17:31 +0200

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/tasks',
+    name: 'tasks',
+    component: TaskManager
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
